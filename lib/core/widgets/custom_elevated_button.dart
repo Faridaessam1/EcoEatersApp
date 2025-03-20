5bd52d07590120c7d5ffa4c7053a5473acbc9835
Faridaessam1 <essamfarida4@gmail.com>
2025-03-20 23:39:51 +0200

    *start eco eaters app

---
+++ lib/core/widgets/custom_elevated_button.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import '../../main.dart';
+import '../constants/app_colors.dart';
+
+class CustomElevatedButton extends StatelessWidget {
+  String? text;
+  Color? buttonColor;
+  Color? textColor;
+  Function()? onPressed;
+  Widget? child;
+  double? fontSize;
+  String? fontFamily;
+  FontWeight? fontWeight;
+  double borderRadius;
+
+  CustomElevatedButton({
+    super.key,
+    this.text,
+    this.buttonColor,
+    this.textColor,
+    this.onPressed,
+    this.child,
+    this.fontFamily,
+    this.fontWeight,
+    this.fontSize = 20,
+    this.borderRadius = 16,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: buttonColor,
+        padding: const EdgeInsets.all(16),
+        shape: RoundedRectangleBorder(
+          side: BorderSide(width: 1, color: AppColors.primaryColor),
+          borderRadius: BorderRadius.circular(borderRadius),
+        ),
+      ),
+      onPressed: onPressed,
+      child: Text(
+        text!,
+        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+          fontWeight: fontWeight,
+          fontSize: fontSize,
+          fontFamily:fontFamily,
+          color: textColor,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      navigatorKey: navigatorKey,
+
+    );
+  }
+}
+
