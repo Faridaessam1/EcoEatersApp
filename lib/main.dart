2e09027c16c12571c0e7fd3466dc9e9cfa395f3d
Faridaessam1 <essamfarida4@gmail.com>
2025-03-27 06:10:52 +0200

    *finish Customer ui *finish app localization package

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
-
 import 'core/routes/app_routes.dart';
 import 'core/routes/routes_name.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+
 final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
@@ -23,6 +25,8 @@ class MyApp extends StatelessWidget {
       builder: EasyLoading.init(
         builder: BotToastInit(),
       ),
+      localizationsDelegates: AppLocalizations.localizationsDelegates,
+      supportedLocales: AppLocalizations.supportedLocales,
 
     );
   }
